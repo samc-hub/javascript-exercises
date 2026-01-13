917b87905bdce303708823e0acf1286c2568e5f0
samc-hub <samuelcox2343@gmail.com>
2026-01-12 21:44:59 -0800

    Use while loop to countdown numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
 
+
+/* const sumAll = function(num1, num2) {
+    let newArr = [] */
+    let i = 10;
+
+    while (i >= 0) {
+        console.log(i);
+    
+    i--;
 };
-
+//console.log(sumAll(1,4));//
 // Do not edit below this line
 module.exports = sumAll;
