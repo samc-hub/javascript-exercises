082257f179e3b2ad28e6700a94efaa1bfc85bc4b
samc-hub <samuelcox2343@gmail.com>
2026-01-12 22:17:20 -0800

    Compute sum of numbers in range regardless of argument order

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -3,16 +3,29 @@ const sumAll = function(num1, num2) {
 
 let newArr = [];
 
-let i = num2;
-    while (i >= num1 ) {
-    newArr.push(i);
+    if (num2 > num1) {
+       let i = num2;
+        while (i >= num1 ) {
+        newArr.push(i);
     
-    i--;
+        i--;
 
     };
+    
+    } else if (num1 > num2) {
+        let i = num1;
+        while (i >= num2 ) {
+        newArr.push(i);
+    
+        i--;
 
+    };
+    }  
+
+    
     return newArr.reduce((sum, current) => sum + current, 0);
 }
+
 //console.log(sumAll(1,4));//
 // Do not edit below this line
 module.exports = sumAll;
