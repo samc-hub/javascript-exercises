46b7f0748ba366f022c9ccb2ab55965771bbcb33
samc-hub <samuelcox2343@gmail.com>
2026-01-12 22:02:38 -0800

    Sum large numbers within range

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,14 +1,18 @@
 
-
 const sumAll = function(num1, num2) {
-    let newArr = [] 
-    let i = 10;
 
-    while (i >= 0) {
-        console.log(i);
+let newArr = [];
+
+let i = num2;
+    while (i >= num1 ) {
+    newArr.push(i);
     
     i--;
-};
+
+    };
+
+    return newArr.reduce((sum, current) => sum + current, 0);
+}
 //console.log(sumAll(1,4));//
 // Do not edit below this line
 module.exports = sumAll;
