d76def9bfbbe04ec6fe747285937ce0b73244a19
samc-hub <samuelcox2343@gmail.com>
2026-01-15 22:11:03 -0800

    convert farenheight to celcius and vice versa

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let number = (num - 32) * (5 / 9);
+  return Math.round(number * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num2) {
+ let number2 = num2 * (9 / 5) + 32;
+  return Math.round(number2 * 10) / 10;
 };
 
 // Do not edit below this line
