6d63299cd0dfdb0101a89cc89942f69f5d1238e4
samc-hub <samuelcox2343@gmail.com>
2026-01-15 21:39:43 -0800

    Run a series of checks to determine if year is a leap year.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
-};
-
+      
+       const leapYears = function(num) {
+        
+        if (num % 4 == 0 && num % 400 == 0) {
+            return true;
+        } else if (num % 4 == 0 && num % 100 !== 0) {
+            return true;
+        } else if (num % 100 == 0 && num % 400 !== 0) {
+            return false; 
+        } else if (num & 100 == 0 && num % 400 == 0) {
+            return true;
+        } else {
+            return false; 
+        }
+    }
 // Do not edit below this line
 module.exports = leapYears;
