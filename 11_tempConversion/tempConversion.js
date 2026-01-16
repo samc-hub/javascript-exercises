99126697c7994a5ebaa0c1593eb88285ed078832
samc-hub <samuelcox2343@gmail.com>
2026-01-15 22:14:28 -0800

    Change variable and argument names to be more representative

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
-const convertToCelsius = function(num) {
-  let number = (num - 32) * (5 / 9);
-  return Math.round(number * 10) / 10;
+const convertToCelsius = function(fahrenheit) {
+  let fahrenheit1 = (fahrenheit - 32) * (5 / 9);
+  return Math.round(fahrenheit1 * 10) / 10;
 };
 
-const convertToFahrenheit = function(num2) {
- let number2 = num2 * (9 / 5) + 32;
-  return Math.round(number2 * 10) / 10;
+const convertToFahrenheit = function(celcius) {
+ let celciusConverted = num2 * (9 / 5) + 32;
+  return Math.round(celciusConverted * 10) / 10;
 };
 
 // Do not edit below this line
