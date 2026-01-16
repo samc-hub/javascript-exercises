235d5342994d06e94da618dde6780a61e9d01084
samc-hub <samuelcox2343@gmail.com>
2026-01-15 21:45:38 -0800

    Fix leap year logic and simplify conditionals

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,17 +1,7 @@
       
-       const leapYears = function(num) {
-        
-        if (num % 4 == 0 && num % 400 == 0) {
-            return true;
-        } else if (num % 4 == 0 && num % 100 !== 0) {
-            return true;
-        } else if (num % 100 == 0 && num % 400 !== 0) {
-            return false; 
-        } else if (num & 100 == 0 && num % 400 == 0) {
-            return true;
-        } else {
-            return false; 
-        }
-    }
+const leapYears = function(year) {
+  return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+};
+
 // Do not edit below this line
 module.exports = leapYears;
