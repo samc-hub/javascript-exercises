1298fe69a7038d4dd96e1c4fb54fd4d0536bed7a
samc-hub <samuelcox2343@gmail.com>
2026-02-17 22:26:52 -0800

    Create a function that will determine if a word or sentence is a palindome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+let punctuation = /[\., ?!]/g;
+let newWord = word.replace(punctuation, "").toLowerCase();
+let reversed = newWord.split("").reverse().join("");
+if (reversed === newWord) {
+    return true;
+} else {
+    return false;
+}};
 
 // Do not edit below this line
 module.exports = palindromes;
